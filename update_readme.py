69931f4ca3d70237a0dd84620db482b77da3f039
Neshie Keplin <78915511+hsratneshsci@users.noreply.github.com>
2024-06-19 09:14:38 +0100

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -1,8 +1,25 @@
 import json
+import requests
 
 # Load config data
 with open('config.json', 'r') as f:
     config = json.load(f)
+
+# Fetch GitHub data
+headers = {'Authorization': f'token {config["GITHUB_TOKEN"]}'}
+user_response = requests.get('https://api.github.com/user', headers=headers)
+repos_response = requests.get('https://api.github.com/user/repos', headers=headers)
+
+user_data = user_response.json()
+repos_data = repos_response.json()
+
+public_repos = len([repo for repo in repos_data if not repo['private']])
+private_repos = len([repo for repo in repos_data if repo['private']])
+total_commits = 0
+
+for repo in repos_data:
+    commits_response = requests.get(repo['commits_url'].replace('{/sha}', ''), headers=headers)
+    total_commits += len(commits_response.json())
 
 # Read the README template
 with open('README_template.md', 'r') as f:
@@ -26,6 +43,11 @@ readme = readme.replace('{{main_projects}}', main_projects)
 
 other_projects = "\n".join([f"- **[{project['name']}]({project['url']})**" for project in config['other_projects']])
 readme = readme.replace('{{other_projects}}', other_projects)
+
+# Update GitHub data
+readme = readme.replace('{{public_repos}}', str(public_repos))
+readme = readme.replace('{{private_repos}}', str(private_repos))
+readme = readme.replace('{{total_commits}}', str(total_commits))
 
 # Write the updated README
 with open('README.md', 'w') as f:
